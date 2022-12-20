7463157f435634affb2556e5e09344a55a5e90c2
asilbekcod <isarvakoriya@gmail.com>
2022-12-20 11:38:24 +0500

    about

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -3,9 +3,11 @@ import Router from '../../routes/routes';
 import Navbar from '../../components/Navbar/Navbar';
 import { Box, Typography } from '@mui/material'
 import Home from '../../pages/Home/Home';
+import About from '../../pages/About/About';
 export default () => (
   <Box>
     <Navbar />
     <Home />
+    <About />
   </Box>
 );
